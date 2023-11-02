a2e1a0b4a8408b9e718537b9695391dde16e2b06
MarIsaTrettel <mariaisabeltrettel@gmail.com>
2023-11-01 23:59:43 -0300

    clase 1

---
+++ Clase1/trettel_unidad1/tipoSeguro.js
@@ -0,0 +1,9 @@
+function enviar (form){
+    var nombre = form.name.value;
+    var email = form.email.value;
+    var mensaje = form.message.value;
+
+    const params ={
+        name: nombre;
+    }
+}
