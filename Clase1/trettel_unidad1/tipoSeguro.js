db2d5aace0e26835ea4c0d99ef578b15d8bf5ec7
MarIsaTrettel <mariaisabeltrettel@gmail.com>
2023-11-03 22:12:58 -0300

    clase 1

--- Clase1/trettel_unidad1/tipoSeguro.js
+++ Clase1/trettel_unidad1/tipoSeguro.js
@@ -1,25 +1,6 @@
-function enviar (form){
-    var nombre = form.name.value;
-    var apellido = form.surname.value;
-    var documento = form.dni.value;
-    var email = form.email.value;
-    var telefono = form.phone.value;
-    var seguro = form.seguro.value;
-
-    const params ={
-        name: nombre,
-        surname: apellido,
-        dni: documento,
-        email: email,
-        phone: telefono,  
-        seguro: seguro,      
-    }
-
-    console.log(params);
-    alert("Su mensaje ha sido enviado correctamente");
-    resetForm();
-}
-
-function resetForm() {
-    document.querySelector("form").reset();
+function apareceTexto(elementoUno, elementoDos, elementoTres) {
+    
+    elementoUno.style.visibility = "visible";
+    elementoDos.style.visibility = "hidden";
+    elementoTres.style.visibility = "hidden";
 }
