8c6efaafd527920bca1910dd8cc635811c6118f1
MarIsaTrettel <mariaisabeltrettel@gmail.com>
2023-11-07 15:34:57 -0300

    clase 2 ejercicio 1

--- Clase1/trettel_unidad1/tipoSeguro.js
+++ Clase1/trettel_unidad1/tipoSeguro.js
@@ -4,3 +4,9 @@ function apareceTexto(elementoUno, elementoDos, elementoTres) {
     elementoDos.style.visibility = "hidden";
     elementoTres.style.visibility = "hidden";
 }
+
+document.getElementById("miFormulario").addEventListener("submit",
+function(event) {
+ event.preventDefault(); // Prevenir el comportamiento por defecto
+ alert("Formulario enviado");
+});
--- Clase2/Ejercicio1.js
+++ Clase2/Ejercicio1.js
@@ -10,3 +10,20 @@ function funcionDos(resultado){
 
 suma (1, 2, funcionDos);
     
+
+/* 
+¿Qué es el tercer parámetro recibido?
+El tercer parámetro recibido es una función callback.
+Una función de callback es una función que se pasa a otra función como un argumento, 
+que luego se invoca dentro de la función externa para completar algún tipo de rutina o acción.
+
+Una función ejecutable «A» se usará como argumento de otra función «B». De esta forma, al llamar 
+a «B», esta ejecutará «A» */
+
+
+/* 
+¿En qué casos es obligatorio desarrollar este tipo de funciones?
+Los callbacks pueden ayudar a no repetir código y a su mantenimiento, a conseguir funciones más específicas 
+y, en ciertos casos, a mejorar el nivel de abstracción y la lectura del código. Pero, donde realmente brillan, las 
+devoluciones de llamada, es en funciones asincrónicas, donde una función tiene que esperar a otra función (como 
+esperar a que se cargue un archivo).*/
